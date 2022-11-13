0c8a4812d76a0aee60345b242aa04daf5345280a
RadiantK <pps8853@gmail.com>
2022-11-13 21:16:36 +0900

    JpaAuditing클래스 설정, BaseEntity생성
    공동으로 사용되는 생성시간, 수정시간을 위해서 BaseEntity를 생성하고 Auditing 설정

---
+++ src/main/java/com/mylog/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.mylog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+// root의 application클래스에 다는 것이 아닌 config를 분리하면 단위테스트 시 필요한 context만 불러올 때 유융
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
