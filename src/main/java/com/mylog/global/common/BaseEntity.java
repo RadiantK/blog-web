bfad3b14d859a44e990013afbf9a61afda8a021a
RadiantK <pps8853@gmail.com>
2023-01-05 13:06:17 +0900

    BaseEntity의 @Data 어노테이션 제거
    사용되지않는 @Data 어노테이션 제거

--- src/main/java/com/mylog/global/common/BaseEntity.java
+++ src/main/java/com/mylog/global/common/BaseEntity.java
@@ -11,7 +11,6 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@Data
 @Getter
 @MappedSuperclass
 @EntityListeners(value = AuditingEntityListener.class)
